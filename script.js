245da7e826f3bbbfe775728fa2c4a57cf8573f16
Alena Maramygina <maratustraa@gmail.com>
2021-10-05 18:41:37 +0500

    создана функция optimizingFunction

--- script.js
+++ script.js
@@ -1,23 +1,17 @@
 /* jshint esversion: 7 */
+'use strict';
 
-// 1) Создать переменную num со значением 266219 (тип данных число).
+const optimizingFunction = function (arg) {
 
-let num = 266219;
+  (typeof arg !== 'string') ? console.log("Это не строка!") : null;
 
-/* 2) Вывести в консоль произведение (умножение) цифр этого числа.
-Например: число 123, при помощи javaScript получить каждую цифру ( 1, 2, 3 ) 
-и перемножить их. Правильно использовать цикл или методы перебора. */
+  arg = arg.trim();
 
-num = String(num).split('');
+  if (arg.length > 30) {
+    return arg.substring(0, 30) + '...';
+  } else {
+    return arg;
+  }
+};
 
-num = num.reduce((previousValue, currentValue) => previousValue * currentValue);
-
-console.log(num);
-
-// 3) Полученный результат возвести в степень 3, используя только 1 оператор (Math.pow не подходит)
-
-num = num ** 3;
-
-// 4) Вывести в консоль первые 2 цифры полученного числа
-
-console.log(String(num).substr(0, 2));
+optimizingFunction();
