456e1fac8e9b779dfec56f1e387e4f8474a52483
Alena Maramygina <maratustraa@gmail.com>
2021-10-14 12:28:38 +0500

    добавлены функции timerOn, dateOptionA и dateOptionB для отображения времения в двух форматах

--- script.js
+++ script.js
@@ -1,32 +1,72 @@
 /* jshint esversion: 7 */
 'use strict';
 
-// 1.
+const timerOn = function () {
 
-let arr = ["12345", "28", "499", "5", "327896", "2076", "0"];
-let newArr = [];
+  let date = new Date();
+  let dayOfMonth = date.getDate();
+  let month = date.getMonth() + 1;
+  let year = date.getFullYear();
+  let weekDay = date.getDay();
+  let hour = date.getHours();
+  let minutes = date.getMinutes();
+  let seconds = date.getSeconds();
+  let weekOptions = { weekday: 'long' };
+  weekDay = date.toLocaleString('ru-RU', weekOptions);
+  let dateResult;
 
-arr.forEach(function (item, index) {
+  const hourForms = ["час", "часа", "часов"];
 
-  let eachNum = arr[index];
 
-  if (+eachNum[0] === 2 || +eachNum[0] === 4) {
-    console.log(eachNum);
-  }
-});
+  const dateOptionA = function () {
 
-// 2.
+    let monthOptions = { month: 'long' };
+    month = date.toLocaleString('ru-RU', monthOptions);
+    month = month.toString().slice(0, -1) + "я";
 
-let maxNumber = 100;
+    dateResult = ("Сегодня " + weekDay + ", " + dayOfMonth + " " + month + " " + year + " года, " + hour + " " + formating() + " " + minutes + " минут " + seconds + " секунд ");
+    console.log(dateResult);
+    return;
+  };
 
-nextPrime:
-for (let i = 2; i <= maxNumber; i++) {
+  const dateOptionB = function () {
 
-  for (let k = 2; k < i; k++) {
-    if (i % k == 0) {
-      continue nextPrime;
+    let monthOptions = { month: 'numeric' };
+    month = date.toLocaleString('ru-RU', monthOptions);
+
+    getZero();
+
+    dateResult = (dayOfMonth + "." + month + "." + year + " - " + hour + ":" + minutes + ":" + seconds);
+    console.log(dateResult);
+    return;
+  };
+
+  const formating = function () {
+
+    if (hour == 1 || hour == 21) {
+      return hourForms[0];
+    } else if (hour >= 5 && hour <= 20 || hour == 0) {
+      return hourForms[2];
+    } else {
+      return hourForms[1];
     }
-  }
-  console.log(i + " Делители этого числа: 1 и " + i);
+  };
 
-}
+  const getZero = function () {
+
+    dayOfMonth = dayOfMonth < 10 ? '0' + dayOfMonth : dayOfMonth;
+    month = month < 10 ? '0' + month : month;
+    hour = hour < 10 ? '0' + hour : hour;
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+    return;
+  };
+
+  dateOptionA();
+  dateOptionB();
+};
+
+setInterval(timerOn, 1000);
+
+
+
