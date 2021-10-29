5f19a962d94703a319c1b9fd03184ff5a40c6a5c
Alena Maramygina <maratustraa@gmail.com>
2021-10-29 19:22:29 +0500

    feat(all): анимация прыгающей картошки

--- script.js
+++ script.js
@@ -1,73 +1,50 @@
-/* jshint esversion: 7 */
-'use strict';
+const potato = document.createElement('div');
+let jumper = 0;
+let direction = "right";
+let screenWidth = document.documentElement.clientWidth - 80;
+let globalID;
 
-const timerOn = function () {
+const startBtn = document.getElementById('start');
+const stopBtn = document.getElementById('stop');
+const resetBtn = document.getElementById('reset');
 
-  let date = new Date();
-  let dayOfMonth = date.getDate();
-  let month = date.getMonth() + 1;
-  let year = date.getFullYear();
-  let weekDay = date.getDay();
-  let hour = date.getHours();
-  let minutes = date.getMinutes();
-  let seconds = date.getSeconds();
-  let weekOptions = { weekday: 'long' };
-  weekDay = date.toLocaleString('ru-RU', weekOptions);
-  let dateResult;
-  const area = document.getElementById('area');
-  const hourForms = ["час", "часа", "часов"];
-  let clockArea1 = document.createElement('div');
-  let clockArea2 = document.createElement('div');
 
-  const dateOptionA = function () {
+potato.textContent = '🥔';
+potato.style.cssText = `cursor: pointer; position: absolute; top: 40px; left: 8px; height: 70px; width: 70px; font-size: 70px;`;
+document.querySelector('body').append(potato);
 
-    let monthOptions = { month: 'long' };
-    month = date.toLocaleString('ru-RU', monthOptions);
-    month = month.toString().slice(0, -1) + "я";
+const movePotato = () => {
+  potato.style.transform = `rotate(${jumper}deg)`;
+  potato.style.left = jumper + 'px';
 
-    dateResult = `Сегодня ${weekDay}, ${dayOfMonth} ${month} ${year} года, ${hour} ${formating()} ${minutes} минут ${seconds} секунд`;
+  switch (direction) {
+    case 'right':
+      jumper += 5;
+      if (jumper >= screenWidth) direction = "left";
+      break;
 
-    clockArea1.textContent = dateResult;
-    area.append(clockArea1);
+    case 'left':
+      jumper -= 5;
+      if (jumper < 0) direction = "right";
+  }
 
-    return;
-  };
-
-  const dateOptionB = function () {
-
-    let monthOptions = { month: 'numeric' };
-    month = date.toLocaleString('ru-RU', monthOptions);
-
-    dateResult = `${getZero(dayOfMonth)}.${getZero(month)}.${year} - ${getZero(hour)}:${getZero(minutes)}:${getZero(seconds)}`;
-
-    clockArea2.textContent = dateResult;
-    area.append(clockArea2);
-
-    return;
-  };
-
-  const formating = function () {
-
-    if (hour == 1 || hour == 21) {
-      return hourForms[0];
-    } else if (hour >= 5 && hour <= 20 || hour == 0) {
-      return hourForms[2];
-    } else {
-      return hourForms[1];
-    }
-  };
-
-  const getZero = function (num) {
-
-    num = num < 10 ? '0' + num : num;
-    return num;
-  };
-
-  dateOptionA();
-  dateOptionB();
+  globalID = requestAnimationFrame(movePotato);
 };
 
-setInterval(timerOn, 1000);
+movePotato();
 
+stopBtn.addEventListener('click', () => {
+  cancelAnimationFrame(globalID);
+});
 
+startBtn.addEventListener('click', () => {
+  if (globalID) cancelAnimationFrame(globalID);
+  movePotato();
+});
 
+resetBtn.addEventListener('click', () => {
+  cancelAnimationFrame(globalID);
+  jumper = 0;
+  potato.style.left = '8px';
+  direction = "right";
+});
